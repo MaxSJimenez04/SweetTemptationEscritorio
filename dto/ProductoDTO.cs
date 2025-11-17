3381a4af5b28061a68fa6af50f8b031f4fce3946
Ness-luna <moralesvanessa2010@gmail.com>
2025-11-16 19:27:58 -0600

    Agregar ProductoService

--- dto/ProductoDTO.cs
+++ dto/ProductoDTO.cs
@@ -8,5 +8,14 @@ namespace sweet_temptation_clienteEscritorio.dto
 {
     internal class ProductoDTO
     {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion {  get; set; }
+        public Decimal Precio {  get; set; }
+        public Boolean Disponible {  get; set; }
+        public int Unidades {  get; set; }
+        public DateTime fechaRegistro {  get; set; }
+        public DateTime fechaModificacion { get; set; }
+        public int categoria {  get; set; }
     }
 }
