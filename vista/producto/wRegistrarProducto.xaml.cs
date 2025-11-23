1f3239445c5e060025861eba71ae9bc5086721a3
Ness-luna <moralesvanessa2010@gmail.com>
2025-11-22 18:39:59 -0600

    Agregar vista de registrar producto

---
+++ vista/producto/wRegistrarProducto.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sweet_temptation_clienteEscritorio.vista.producto
+{
+    /// <summary>
+    /// Lógica de interacción para wRegistrarProducto.xaml
+    /// </summary>
+    public partial class wRegistrarProducto : Page
+    {
+        public wRegistrarProducto()
+        {
+            InitializeComponent();
+        }
+    }
+}
